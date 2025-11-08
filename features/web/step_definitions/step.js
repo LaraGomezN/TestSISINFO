8f186f36ed4071217e953e6658767ea235a7b6fc
LaraGomezN <n.lara@uniandes.edu.co>
2025-11-07 19:58:58 -0500

    first commit

---
+++ features/web/step_definitions/step.js
@@ -0,0 +1,80 @@
+const { Given,When,Then} = require('@cucumber/cucumber');
+const LoginPage = require('../pageObject/loginPage');
+const RolesPage = require('../pageObject/rolesPage');
+const ProyectosPage = require('../pageObject/proyectosGradoPage');
+//Login steps
+When('I login with {string} and {string}', async function (username, password) {
+    const loginPage = new LoginPage(this.driver);
+    await loginPage.open();
+    await loginPage.login(username, password);
+    await loginPage.pauseForLogin(2000);
+});
+
+//Roles steps
+When ('I go to Roles section', async function () { 
+    const rolesPage = new RolesPage(this.driver);
+    await rolesPage.goToRoles();
+}); 
+
+When ('I search for user {string}', async function (username) { 
+    const rolesPage = new RolesPage(this.driver);
+    await rolesPage.searchUser(username);
+});
+
+When ('I click in role assign', async function () { 
+    const rolesPage = new RolesPage(this.driver);
+    await rolesPage.goToAsignarRoles();
+});
+
+When ('I assign profesor role', async function () { 
+    const rolesPage = new RolesPage(this.driver);
+    await rolesPage.assignProfesorRole();
+});
+
+Then ('I check profesor role assigned', async function () { 
+    const rolesPage = new RolesPage(this.driver);
+    await rolesPage.checkProfesorRoleAssigned();
+});
+
+Then ('I remove profesor role', async function () { 
+    const rolesPage = new RolesPage(this.driver);
+    await rolesPage.assignProfesorRole();
+});
+
+
+When ('I assign coordinador role', async function () { 
+    const rolesPage = new RolesPage(this.driver);
+    await rolesPage.assignCoordinadorRole();
+});
+
+Then ('I check coordinador role assigned', async function () { 
+    const rolesPage = new RolesPage(this.driver);
+    await rolesPage.checkCoordinadorRoleAssigned();
+});
+
+Then ('I remove coordinador role', async function () { 
+    const rolesPage = new RolesPage(this.driver);
+    await rolesPage.assignCoordinadorRole();
+});
+
+
+When ('I clear search input', async function () { 
+    const rolesPage = new RolesPage(this.driver);
+    await rolesPage.clearSearch();
+});
+
+//Proyectos steps
+
+
+
+
+When ('I go to Proyectos de Grado section', async function () { 
+    const proyectosPage = new ProyectosPage(this.driver);
+    await proyectosPage.goToProyectosDeGrado();
+    await proyectosPage.goToPublicarConsultarProyectos();
+});
+
+When ('I go to Crear Tema de Proyecto section', async function () { 
+    const proyectosPage = new ProyectosPage(this.driver);
+    await proyectosPage.goToCrearTemaProyecto();
+}   );
